a3f2d2e0de8d7abc39f67c967ec4bef1c00ab71b
Tolu A <mrtalder@gmail.com>
2024-03-22 12:07:52 +0000

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(provivdedWord, num) {
+    let word = '';
+    if (num < 0){
+        return 'ERROR'
+    }
+    for (let i = 0; i<num; i++){
+        word += provivdedWord;
+    }
+    return word;
 };
 
 // Do not edit below this line
