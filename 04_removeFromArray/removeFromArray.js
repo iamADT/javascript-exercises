f3d2fd22523073661dd2543e479c1a229f95cfa7
Tolu A <mrtalder@gmail.com>
2024-03-23 10:34:40 +0000

    Update Exercise 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(providedArray, ...valuesToBeRemoved) {
+    let modifiedArray = providedArray.filter(function(item){
+        return !valuesToBeRemoved.includes(item)
+    })
+    return modifiedArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    let sum = 0;
+    let num1;
+    let num2;
+    
+    if((typeof(arg1) !== "number") || (typeof(arg2) !== "number")){
+        return 'ERROR';
+    }
+    else if(arg1 < 0 || arg2 < 0){
+        return 'ERROR';
+    }
+    else if (arg2 > arg1){
+        num1 = arg1;
+        num2 = arg2;
+    } else if (arg1 > arg2) {
+        num1 = arg2;
+        num2 = arg1;
+    }
 
+    for (let i = num1; i <= num2; i++){
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
